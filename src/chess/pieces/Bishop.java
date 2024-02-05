73b56b8de83172b9d4a3df3992171fe2ed5685e7
Renato <renatobryant@gmail.com>
2024-02-05 20:11:53 -0300

    Litle improvement in board printing

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -4,15 +4,15 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
+public class Bishop extends ChessPiece{
 
-    public King( Board board, Color color ){
-        super(board, color);
+    public Bishop( Board board, Color color ) {
+        super( board, color );
     }
 
     @Override
     public String toString() {
-        return "K";
+        return "B";
     }
     
 }
--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -4,15 +4,15 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
-
-    public King( Board board, Color color ){
-        super(board, color);
+public class Pawn extends ChessPiece{
+    
+    public Pawn( Board board, Color color ){
+        super( board, color );
     }
 
     @Override
     public String toString() {
-        return "K";
+        return "P";
     }
-    
+
 }
